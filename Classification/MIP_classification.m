cb05eb96a91b5db03fdbadab796122a46cf6fa3b
Pablo Ugalde Salas <pablo.ugalde.s@gmail.com>
2019-08-18 16:39:15 +0200

    added main

---
+++ Classification/MIP_classification.m
@@ -0,0 +1,47 @@
+function [a, b, y_AOB, y_NOB, error_AOB, error_NOB] = MIP_classification(partialWeights, xG1, xG2,yA_ref,yB_ref,delta)
+n = length(partialWeights(:,1));
+T = length(partialWeights(1,:));
+M1 = 1/(yA_ref*(1-delta)); %Reference Parameter Table 3 Weissmann et al 1994
+M2 = 1/(yB_ref*(1-delta)); %big M constraints.
+m1 = 1/(yA_ref*(1+delta)); 
+m2 = 1/(yB_ref*(1+delta));
+A = [-eye(T) zeros(T) partialWeights' zeros(T,3*n);
+    -eye(T) zeros(T) -partialWeights' zeros(T,3*n);
+    zeros(T) -eye(T) zeros(T,n) partialWeights' zeros(T,2*n);
+    zeros(T) -eye(T) zeros(T,n) -partialWeights' zeros(T,2*n);
+    zeros(n,2*T) eye(n) zeros(n) -M1*eye(n) zeros(n);
+    zeros(n,2*T)  zeros(n) eye(n) zeros(n) -M2*eye(n) ;
+    zeros(n,2*T) -eye(n) zeros(n) m1*eye(n) zeros(n);
+    zeros(n,2*T)  zeros(n) -eye(n) zeros(n) m2*eye(n) ;
+    zeros(n,2*T+2*n) eye(n) eye(n)];
+b = [xG1; -xG1; xG2; -xG2; zeros(4*n,1); ones(n,1)];
+lb = zeros(2*T+4*n,1);
+ub = [inf*ones(2*T,1); M2*ones(2*n,1) ;ones(2*n,1)];
+intcon = 2*T+ 2*n+1:2*T+4*n;
+variables_type = '';
+for i=1:2*T+ 2*n
+    variables_type = strcat(variables_type,'C');
+end
+for i=intcon
+    variables_type = strcat(variables_type,'B');
+end
+model.A = sparse(A);
+model.Q = sparse(blkdiag(eye((2*T)),zeros(4*n,4*n)));
+% model.obj = [ones(2*T,1); zeros(4*n,1)];
+model.rhs = b;
+model.sense = '<';
+model.vtype = variables_type;
+model.modelsense = 'min';
+model.lb = lb;
+model.ub = ub;
+gurobi_write(model, 'mip1.lp');
+params.outputflag = 0;
+result = gurobi(model, params);
+sol = result.x;
+error_AOB = sol(1:T);
+error_NOB = sol(T+1:2*T);
+y_AOB = sol(2*T+1:2*T+n);
+y_NOB = sol(2*T+n+1:2*T+2*n);
+a = sol(2*T+2*n+1:2*T+3*n);
+b = sol(2*T+3*n+1:end);
+end
---
+++ Classification/main.m
@@ -0,0 +1,54 @@
+clear
+addpath('..\\General Functions')
+reactor = 'A';
+delta = 0.01; %Between 0 and 1
+id = 'Classification_Try';
+file_name = sprintf('Classification_algorithm_%s',id);
+[t_obs,X,s_in,S1,S2,S3,dilution_rate] = load_biomass(reactor);
+[t_OTU,OTU_rel] = load_relative_abundance(reactor);
+OTU_rel = diag(1./sum(OTU_rel,2))*OTU_rel;
+number_OTU = length(OTU_rel(1,:));
+minimal_abundance = 0;
+t_change = 183; % From dumont et al
+time_steps_index = find(t_obs > t_change & t_obs < t_change +50);
+time_steps_index_OTU = find(t_OTU > t_change & t_OTU < t_change +50);
+% timeStepsIndex = 1:length(tObs);
+OTU_series = timeseries(OTU_rel(time_steps_index_OTU,:),...
+    t_OTU(time_steps_index_OTU));
+mean_abundance = mean(OTU_series,'Weighting','time')';
+max_abundance =  max(OTU_series)';
+considered_OTU = find(max_abundance > minimal_abundance);
+% consideredOTU = find(presenceTime(:,3) > minimumDays);
+OTU_interp = interpolate_biomass(t_obs,X,t_OTU,OTU_rel);
+OTU_interp_fun = @(t) interp1(t_OTU,OTU_interp(:,considered_OTU),t);
+d_interp = @(t) interp1(t_obs,dilution_rate,t);
+s_in_interp = @(t) interp1(t_obs,s_in,t);
+s1_interp = @(t) interp1(t_obs,S1,t);
+s2_interp = @(t) interp1(t_obs,S2,t);
+dynamic = @(t,z) -1*d_interp(t)*(z - s_in_interp(t));
+t0 = t_obs(time_steps_index(1));
+tF = t_obs(time_steps_index(end));
+[T, Z] = ode15s(dynamic,[t0 tF],S1(time_steps_index(1)));
+invariant = @(t) interp1(T,Z,t);
+partial_weights = OTU_interp_fun(t_obs(time_steps_index))';
+Z1 = invariant(t_obs(time_steps_index));
+% xG1 = max(0,Z1 - S1);
+% xG2 = max(0,Z1 - S1 - S2);
+xG1 = Z1 - S1(time_steps_index);
+xG2 = Z1 - S1(time_steps_index) - S2(time_steps_index);
+yA_ref = 0.251;
+yB_ref = 0.062;
+[a, b, y_AOB, y_NOB, error_AOB, error_NOB] = MIP_classification(partial_weights, xG1, xG2,yA_ref,yB_ref,delta);
+classAOB = zeros(number_OTU,1);
+classNOB = zeros(number_OTU,1);
+yieldsAOB = zeros(number_OTU,1);
+yieldsNOB = zeros(number_OTU,1);
+classAOB(considered_OTU) = round(a);
+classNOB(considered_OTU) = round(b);
+yieldsAOB(considered_OTU) = y_AOB;
+yieldsNOB(considered_OTU) = y_NOB;
+% yieldsAOB(yieldsAOB == inf) = 0;
+% yieldsNOB(yieldsNOB == inf) = 0;
+% classification = [(1:numberOfOTU)' classAOB classNOB yieldsAOB yieldsNOB meanAbundance maxAbundance];
+% errors = [tObs errorAOB errorNOB partialWeights'*yieldsA partialWeights'*yieldsB S1 S2 Sin];
+save(sprintf('MAT_files\\%s',file_name));
