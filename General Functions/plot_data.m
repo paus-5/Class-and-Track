afe2d75b0eb4786a4105458c7fc4f2375e73e90e
Pablo Ugalde Salas <pablo.ugalde.s@gmail.com>
2019-10-18 15:14:31 +0200

    changed variable names

--- General Functions/plot_data.m
+++ General Functions/plot_data.m
@@ -2,28 +2,28 @@ clear
 close all
 addpath('..\\General Functions')
 Reactor = 'B';
-[tObs,X,sIn,s1,s2,s3,dilutionRate] = load_biomass(Reactor);
-[tOTU,OTU] = load_relative_abundance(Reactor);
+[t_obs,biomass,s_in,s1,s2,s3,dilutionRate] = load_biomass(Reactor);
+[t_OTU,OTU] = load_relative_abundance(Reactor);
 subplot(4,1,1),
-plot(tObs,X,'LineWidth',3)
+plot(t_obs,biomass,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12}Concentration [g/l]'),...
     title(sprintf('Total Biomass in Time Reactor %s',Reactor))
     set(gca,'fontsize',15);
 subplot(4,1,2),
-plot(tObs,s1,'LineWidth',3)
+plot(t_obs,s1,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12}Concentration [g/l]'),...
     title(sprintf('Total ammonium in Time Reactor %s',Reactor))
     set(gca,'fontsize',15);
 subplot(4,1,3),
-plot(tObs,s2,'LineWidth',3)
+plot(t_obs,s2,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12}Concentration [g/l]'),...
     title(sprintf('Total nitrite in Time Reactor %s',Reactor))
     set(gca,'fontsize',15);
 subplot(4,1,4),
-plot(tObs,s3,'LineWidth',3)
+plot(t_obs,s3,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12}Concentration [g/l]'),...
     title(sprintf('Total nitrate in Time Reactor %s',Reactor))
@@ -35,13 +35,13 @@ fileOut = sprintf('Data Reactor %s',Reactor);
 % print(sprintf('..\\Images\\%s',fileOut),'-dpng','-r0')
 figure
 subplot(2,1,1),
-plot(tObs,sIn,'LineWidth',3)
+plot(t_obs,s_in,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12}Concentration [g/l]'),...
     title(sprintf('Input ammonium in Time Reactor %s',Reactor))
     set(gca,'fontsize',15);
 subplot(2,1,2),
-plot(tObs,dilutionRate,'LineWidth',3)
+plot(t_obs,dilutionRate,'LineWidth',3)
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12} Rate [day]^{-1}'),...
     title(sprintf('Dilution Rate in Time Reactor %s',Reactor))
@@ -49,7 +49,7 @@ plot(tObs,dilutionRate,'LineWidth',3)
 figure
 numberOfOTU = length(OTU(1,:));
  colors = mat2cell(jet(numberOfOTU), ones(1,numberOfOTU), 3);
- OTUPlot = plot(tOTU,OTU,'LineWidth',1.5);
+ OTUPlot = plot(t_OTU,OTU,'LineWidth',1.5);
     xlabel('\fontsize{12}Time [days]'),...
     ylabel('\fontsize{12} % of Abundance'),...
     title(sprintf('Relative abundance of OTU in Time Reactor %s',Reactor))
