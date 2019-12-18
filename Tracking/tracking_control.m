753ef46378398bbdde65c20133808f3590932c21
Pablo Ugalde Salas <pablo.ugalde.s@gmail.com>
2019-12-18 16:24:20 +0100

    augmented the threshold for the contrl

--- Tracking/tracking_control.m
+++ Tracking/tracking_control.m
@@ -1,5 +1,5 @@
 function v = tracking_control(lambda,B,P,X,sF,n)
 B1_vec = diag(B(1:n,:));
 x_trunc = X(1:n);
-v = max(-1,min(-1./lambda.*B1_vec.*(P.*x_trunc - sF),0.2));
+v = max(-1,min(-1./lambda.*B1_vec.*(P.*x_trunc - sF),0.5));
 end
