4c1e31db8b55dabc25d17184e1fb3043f3a88ead
Pablo Ugalde Salas <pablo.ugalde.s@gmail.com>
2020-01-16 18:04:39 +0100

    reverting the experiment

--- Tracking/Ax.m
+++ Tracking/Ax.m
@@ -2,8 +2,8 @@ function A = Ax(X,nA,nB,kA,kB,muA,muB,kSA,kSB,kI,sIn,dilution_rate)
 addpath('..//');
 n = nA+ nB;
 % growthRowVector = [growthMonod(X(n+1),muA,kSA)' growthHaldane(X(n+2),muB,kSB,kI)'];
-% growth_row_vector = [growth_monod(X(n+1),muA,kSA)' growth_monod(X(n+2),muB,kSB)'];
-growth_row_vector = [muA ; muB]';
+growth_row_vector = [growth_monod(X(n+1),muA,kSA)' growth_monod(X(n+2),muB,kSB)'];
+% growth_row_vector = [muA ; muB]';
 A = [diag(growth_row_vector-dilution_rate) zeros(n,3);
     [-kA; zeros(nB,1)]'.*growth_row_vector (sIn/X(n+1) -1)*dilution_rate 0 0; 
     [kA; -kB]'.*growth_row_vector 0 -dilution_rate 0;
